bd0208ee5ef9d66816fcccaf8c34c2db68ec918f
Code Gummy <codegummy33@gmail.com>
2024-10-30 16:05:51 +0100

    Updated site

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
 const words = ["Samantha", "an Engineer", "a Creative", "Innovative"];
 const typingTextElement = document.querySelector(".typing-text");
-const menu = document.querySelector(".menu")
-const navbar = document.querySelector(".navbar-small .navlinks")
+const menu = document.querySelector(".menu");
+const navbar = document.querySelector(".navbar-small .navlinks");
 
 let wordIndex = 0;
 let letterIndex = 0;
 
-function type(){
-    if(letterIndex < words[wordIndex].length){
-        typingTextElement.textContent +=  `${words[wordIndex].charAt(letterIndex)}`
-        
-        letterIndex++
-        setTimeout(type,100)
-        
-    }else{
-        setTimeout(erase,2000)
-    }
+function type() {
+  if (letterIndex < words[wordIndex].length) {
+    typingTextElement.textContent += `${words[wordIndex].charAt(letterIndex)}`;
+
+    letterIndex++;
+    setTimeout(type, 100);
+  } else {
+    setTimeout(erase, 2000);
+  }
 }
 
-function erase(){
-    if(letterIndex > 0){
-        
-        typingTextElement.textContent =  words[wordIndex].substring(0, letterIndex-1)
-        letterIndex--
-        setTimeout(erase,50)
-    }else{
-        wordIndex = (wordIndex + 1) % words.length;
-        setTimeout(type, 500); 
-    }
-
+function erase() {
+  if (letterIndex > 0) {
+    typingTextElement.textContent = words[wordIndex].substring(
+      0,
+      letterIndex - 1
+    );
+    letterIndex--;
+    setTimeout(erase, 50);
+  } else {
+    wordIndex = (wordIndex + 1) % words.length;
+    setTimeout(type, 500);
+  }
 }
 
-function toggleNavbar(){
-    navbar.style.display  = (navbar.style.display == "block"  ) ?"none" : "block"
-   
-
+function toggleNavbar() {
+  navbar.style.display = navbar.style.display == "block" ? "none" : "block";
 }
-document.addEventListener("DOMContentLoaded", function(){ setTimeout(type, 500)})
-menu.addEventListener("click", toggleNavbar)
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(type, 500);
+});
+menu.addEventListener("click", toggleNavbar);
