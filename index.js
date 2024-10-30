18033a4ee0a22fc66a1ab10e7b69870790879b2a
Code Gummy <codegummy33@gmail.com>
2024-10-30 14:33:43 +0100

    Updated site

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const words = ["Samantha", "an Engineer", "a Creative", "Innovative"];
 const typingTextElement = document.querySelector(".typing-text");
+const menu = document.querySelector(".menu")
+const navbar = document.querySelector(".navbar-small .navlinks")
 
 let wordIndex = 0;
 let letterIndex = 0;
@@ -29,4 +31,10 @@ function erase(){
 
 }
 
+function toggleNavbar(){
+    navbar.style.display  = (navbar.style.display == "block"  ) ?"none" : "block"
+   
+
+}
 document.addEventListener("DOMContentLoaded", function(){ setTimeout(type, 500)})
+menu.addEventListener("click", toggleNavbar)
