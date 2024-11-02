ac3a5704922baf6d331e01e8e4af8a4fd4c5e079
Code Gummy <codegummy33@gmail.com>
2024-11-02 14:16:35 +0100

    u

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const words = ["Samantha", "an Engineer", "a Creative", "Innovative"];
 const typingTextElement = document.querySelector(".typing-text");
 const menu = document.querySelector(".menu");
 const navbar = document.querySelector(".navbar-small .navlinks");
+const blogpost = document.querySelectorAll(".blog-post");
 
 let wordIndex = 0;
 let letterIndex = 0;
@@ -38,3 +39,10 @@ document.addEventListener("DOMContentLoaded", function () {
   setTimeout(type, 500);
 });
 menu.addEventListener("click", toggleNavbar);
+
+blogpost.forEach((post) => {
+  post.addEventListener("click", () => {
+    const url = post.getAttribute("data-url");
+    window.location.href = url;
+  });
+});
