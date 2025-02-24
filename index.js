27e2da8e36ba411633abef45ed3a86f43ac8b10d
Code Gummy <codegummy33@gmail.com>
2025-02-24 21:27:41 +0100

    added-toggle

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const navbar = document.querySelector(".navbar-small .navlinks");
 const blogpost = document.querySelectorAll(".blog-post");
 const general_Item = document.querySelectorAll(".general-item");
 const logo = document.querySelectorAll(".logo");
+const toggleButton = document.querySelectorAll(".toggle-theme");
 
 let wordIndex = 0;
 let letterIndex = 0;
@@ -54,6 +55,49 @@ general_Item.forEach((item) => {
     if (item.getAttribute("data-url")) {
       const url = item.getAttribute("data-url");
       window.location.href = url;
+    }
+  });
+});
+
+toggleButton.forEach((item) => {
+  item.addEventListener("click", function () {
+    const root = document.documentElement;
+
+    if (root.style.getPropertyValue("--bg-color") === "#fff") {
+      root.style.setProperty("--bg-color", "#121212");
+      root.style.setProperty("--text-color", "#fff");
+      localStorage.setItem("theme", "dark");
+    } else {
+      root.style.setProperty("--bg-color", "#fff");
+      root.style.setProperty("--text-color", "#000000");
+      localStorage.setItem("theme", "light");
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const root = document.documentElement;
+  const savedTheme = localStorage.getItem("theme");
+  const themeIcon = document.getElementById("theme-icon");
+
+  if (savedTheme === "dark") {
+    root.style.setProperty("--bg-color", "#121212");
+    root.style.setProperty("--text-color", "#ffffff");
+    root.classList.add("dark-mode");
+    themeIcon.textContent = "🌙";
+  } else {
+    root.style.setProperty("--bg-color", "#fff");
+    root.style.setProperty("--text-color", "#000000");
+  }
+  toggleButton.addEventListener("click", function () {
+    if (root.classList.contains("dark-mode")) {
+      root.classList.remove("dark-mode");
+      themeIcon.textContent = "🌞"; // Sun for light mode
+      localStorage.setItem("theme", "light");
+    } else {
+      root.classList.add("dark-mode");
+      themeIcon.textContent = "🌙"; // Moon for dark mode
+      localStorage.setItem("theme", "dark");
     }
   });
 });
